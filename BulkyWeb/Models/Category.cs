f0b6968ca600321524a1befeac71cebb2cb5c830
Taylor Lo <taylorloph@gmail.com>
2024-01-24 22:58:59 -0500

    Add Data Annotations

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,8 +1,12 @@
-﻿namespace BulkyWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
     }
