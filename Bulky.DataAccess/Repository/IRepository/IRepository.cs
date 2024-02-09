9bdfe3a7239a0c781def72909a6c28fe5e47236c
Taylor Lo <taylorloph@gmail.com>
2024-02-09 12:32:40 -0500

    Implement Repository Interface

--- Bulky.DataAccess/Repository/IRepository/IRepository.cs
+++ Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -13,7 +13,6 @@ namespace Bulky.DataAccess.Repository.IRepository
         IEnumerable<T> GetAll();
         T Get(Expression<Func<T, bool>> filter);
         void Add(T entity);
-        void Update(T entity);
         void Remove(T entity);
         void RemoveRange(IEnumerable<T> entity);
     }
