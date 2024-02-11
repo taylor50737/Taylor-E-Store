a45c98a7ad08bd56f186dc4fe7bfa1de1b5cc2fb
Taylor Lo <taylorloph@gmail.com>
2024-02-11 12:42:34 -0500

    UnitOfWork Implementation

--- Bulky.DataAccess/Repository/CategoryRepository.cs
+++ Bulky.DataAccess/Repository/CategoryRepository.cs
@@ -11,11 +11,6 @@ namespace BulkyBook.DataAccess.Repository
         {
             _db = db;
         }   
-        public void Save()
-        {
-            _db.SaveChanges();
-        }
-
         public void Update(Category obj)
         {
             _db.Categories.Update(obj);
