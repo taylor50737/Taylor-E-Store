c7bb048d1daa85bc3a13ea1a99bec61f45f30078
Taylor Lo <taylorloph@gmail.com>
2024-02-16 12:57:33 -0500

    Add Image Url Column

--- Bulky.Models/Product.cs
+++ Bulky.Models/Product.cs
@@ -43,5 +43,6 @@ namespace BulkyBook.Models
         public int CategoryId { get; set; }
         [ForeignKey("CategoryId")]
         public Category Category { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
