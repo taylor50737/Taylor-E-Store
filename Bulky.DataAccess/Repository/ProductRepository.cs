4b438227b9af6dfdffbb4a97178c0322f1afd314
Taylor Lo <taylorloph@gmail.com>
2024-02-16 11:56:54 -0500

    Product Repository and UnitOfWork

---
+++ Bulky.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db) 
+        {
+            _db = db;
+        }   
+        public void Update(Product obj)
+        {
+            _db.Products.Update(obj);
+        }
+    }
+}
