c05b5a1307c11d49469b0a5cc084feca7687e820
Taylor Lo <taylorloph@gmail.com>
2024-02-18 13:36:41 -0500

    Add ApplicationUser Repository

---
+++ Bulky.DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        private ApplicationDbContext _db;
+        public ApplicationUserRepository(ApplicationDbContext db) : base(db) 
+        {
+            _db = db;
+        }
+    }
+}
