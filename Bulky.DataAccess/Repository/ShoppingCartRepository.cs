f436dd29c0f9789839e1054fed013c69440f5f3a
Taylor Lo <taylorloph@gmail.com>
2024-02-18 13:23:36 -0500

    Add Shopping Cart to Repository

---
+++ Bulky.DataAccess/Repository/ShoppingCartRepository.cs
@@ -0,0 +1,19 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class ShoppingCartRepository : Repository<ShoppingCart>, IShoppingCartRepository
+    {
+        private ApplicationDbContext _db;
+        public ShoppingCartRepository(ApplicationDbContext db) : base(db) 
+        {
+            _db = db;
+        }   
+        public void Update(ShoppingCart obj)
+        {
+            _db.ShoppingCarts.Update(obj);
+        }
+    }
+}
