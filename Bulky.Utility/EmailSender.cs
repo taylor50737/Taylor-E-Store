7e3ea2261f84407874a922ad97ac266ddc5d9b3f
Taylor Lo <taylorloph@gmail.com>
2024-02-20 19:33:31 -0500

    SendGrid Email Setup

--- Bulky.Utility/EmailSender.cs
+++ Bulky.Utility/EmailSender.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +12,20 @@ namespace BulkyBook.Utility
 {
     public class EmailSender : IEmailSender
     {
+        public string SendGridSecret { get; set; }
+        public EmailSender(IConfiguration _config) 
+        {
+            SendGridSecret = _config.GetValue<string>("SendGrid:SecretKey");
+        }
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            return Task.CompletedTask;
+            var client = new SendGridClient(SendGridSecret);
+
+            var from = new EmailAddress("phlo1@myseneca.ca", "Taylor E-Store");
+            var to = new EmailAddress(email);
+            var message = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
+
+            return client.SendEmailAsync(message);
         }
     }
 }
