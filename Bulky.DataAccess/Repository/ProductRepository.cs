5dbbb824c3dd596b79b67909cb1ae0327736ff74
Taylor Lo <taylorloph@gmail.com>
2024-02-23 12:04:43 -0500

    Upload Images on Product Upsert

--- Bulky.DataAccess/Repository/ProductRepository.cs
+++ Bulky.DataAccess/Repository/ProductRepository.cs
@@ -25,6 +25,7 @@ namespace BulkyBook.DataAccess.Repository
                 objFromDb.Description = obj.Description;
                 objFromDb.CategoryId = obj.CategoryId;
                 objFromDb.Author = obj.Author;
+                objFromDb.ProductImages = obj.ProductImages;
             }
         }
     }
