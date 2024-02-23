93436618009559e8a2d4beffde72b998fc8e6ad3
Taylor Lo <taylorloph@gmail.com>
2024-02-23 11:34:18 -0500

    Add Product Image Repository

---
+++ Bulky.DataAccess/Repository/ProductImageRepository.cs
@@ -0,0 +1,19 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class ProductImageRepository : Repository<ProductImage>, IProductImageRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductImageRepository(ApplicationDbContext db) : base(db) 
+        {
+            _db = db;
+        }   
+        public void Update(ProductImage obj)
+        {
+            _db.ProductImages.Update(obj);
+        }
+    }
+}
--- Bulky.DataAccess/Repository/UnitOfWork.cs
+++ Bulky.DataAccess/Repository/UnitOfWork.cs
@@ -18,6 +18,7 @@ namespace BulkyBook.DataAccess.Repository
         public IApplicationUserRepository ApplicationUser { get; private set; }
         public IOrderHeaderRepository OrderHeader { get; private set; }
         public IOrderDetailRepository OrderDetail { get; private set; }
+        public IProductImageRepository ProductImage { get; private set; }
 
         public UnitOfWork(ApplicationDbContext db)
         {
@@ -29,6 +30,7 @@ namespace BulkyBook.DataAccess.Repository
             ApplicationUser = new ApplicationUserRepository(_db);
             OrderHeader = new OrderHeaderRepository(_db);
             OrderDetail = new OrderDetailRepository(_db);
+            ProductImage = new ProductImageRepository(_db);
         }
 
         public void Save()
